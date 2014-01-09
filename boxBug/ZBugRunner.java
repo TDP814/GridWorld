297a0e929be0fd9b70c46917beca769bb3ada4ba
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2014-01-09 09:30:16 -0700

    Updated

---
+++ boxBug/ZBugRunner.java
@@ -0,0 +1,26 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+/**
+ * This class runs a world that contains box bugs. <br />
+ * This class is not tested on the AP CS A and AB exams.
+ */
+public class ZBugRunner
+{
+    public static void main(String[] args)
+    {
+        ActorWorld world = new ActorWorld();
+        //BoxBug alice = new BoxBug(6);
+        //alice.setColor(Color.ORANGE);
+        //CircleBug bob = new CircleBug(1);
+        //SpiralBug tom = new SpiralBug(1);
+        ZBug brad = new ZBug(5);
+        //world.add(new Location(7, 8), alice);
+        //world.add(new Location(5, 5), bob);
+        //world.add(new Location(9, 9), tom);
+        world.add(new Location(1, 1), brad);
+        world.show();
+    }
+}
