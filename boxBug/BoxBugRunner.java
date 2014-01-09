dfc5afcaea66c79a8e0f360e55e834a2a0b29918
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2014-01-09 09:27:05 -0700

    Complete

--- boxBug/BoxBugRunner.java
+++ boxBug/BoxBugRunner.java
@@ -30,13 +30,15 @@ public class BoxBugRunner
     public static void main(String[] args)
     {
         ActorWorld world = new ActorWorld();
-        BoxBug alice = new BoxBug(6);
-        alice.setColor(Color.ORANGE);
-        CircleBug bob = new CircleBug(1);
-        SpiralBug tom = new SpiralBug(1);
-        world.add(new Location(7, 8), alice);
-        world.add(new Location(5, 5), bob);
-        world.add(new Location(9, 9), tom);
+        //BoxBug alice = new BoxBug(6);
+        //alice.setColor(Color.ORANGE);
+        //CircleBug bob = new CircleBug(1);
+        //SpiralBug tom = new SpiralBug(1);
+        ZBug brad = new ZBug(5);
+        //world.add(new Location(7, 8), alice);
+        //world.add(new Location(5, 5), bob);
+        //world.add(new Location(9, 9), tom);
+        world.add(new Location(1, 1), brad);
         world.show();
     }
 }
