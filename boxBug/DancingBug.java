443af68109b3ef3d18124b82f7a45923413f9022
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2014-01-14 09:08:23 -0700

    Almost Done

---
+++ boxBug/DancingBug.java
@@ -0,0 +1,30 @@
+import info.gridworld.actor.Bug;
+
+public class DancingBug extends Bug
+{
+    private int steps;
+    private int sideLength;
+
+    /**
+     * Constructs a box bug that traces a square of a given side length
+     * @param length the side length
+     */
+    public DancingBug(int[] random)
+    {
+        steps = 0;
+        sideLength = length;
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    public void act()
+    {
+        int turns=0;
+        turns=random[];
+
+        super.act();
+        
+
+    }
+}
