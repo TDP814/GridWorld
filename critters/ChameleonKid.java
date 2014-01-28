e570f9a09d57862581f267774cf317a972e5c1ee
Perkins <Perkinstd@s.dcsdk12.org>
2014-01-28 08:42:38 -0700

    Updated

---
+++ critters/ChameleonKid.java
@@ -0,0 +1,10 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+import java.util.ArrayList;
+
+public class ChameleonKid extends ChameleonCritter {
+	
+}
