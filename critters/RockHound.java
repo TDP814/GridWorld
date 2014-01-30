215b84d87ccc20ce66a5abfdd0d2ca29caa8f5cf
Perkins <Perkinstd@s.dcsdk12.org>
2014-01-30 08:34:48 -0700

    Fixed and working

--- critters/RockHound.java
+++ critters/RockHound.java
@@ -2,10 +2,11 @@ import info.gridworld.actor.Actor;
 import info.gridworld.actor.Rock;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
+import java.util.ArrayList;
 
 public class RockHound extends Critter {
 	
-	public void processActos(ArrayList<Actor> actors){
+	public void processActors(ArrayList<Actor> actors){
 		for (Actor a: actors) {
 			if (a instanceof Rock) {
 				a.removeSelfFromGrid();
