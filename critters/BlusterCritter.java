33853f81574d7f3a25074140838f2de60d719b51
Perkins <Perkinstd@s.dcsdk12.org>
2014-01-30 08:56:46 -0700

    Work in Process

--- critters/BlusterCritter.java
+++ critters/BlusterCritter.java
@@ -1,4 +1,5 @@
 import info.gridworld.actor.Actor;
+import info.gridworld.actor.Rock;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 import java.awt.Color;
@@ -7,8 +8,33 @@ import java.util.ArrayList;
 
 
 public class BlusterCritter extends Critter{
+
+    private int courageFactor;
+
+    public BlusterCritter(int c){
+        super();
+        courageFactor = c;
+    }
+
+    public ArrayList<Actor> getActors(){
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+
+        Location loc = getLocation();
+        for (int  r = loc.getRow() - 2; r<=loc.getRow()+2; r++) {
+            for (int c = loc.getCol()-2; c<=loc.getCol()+2; c++) {
+                Location tempLoc = new Location(r, c);
+                if (getGrid().isValid(tempLoc)) {
+                    Actor a = getGrid().get(tempLoc);
+                    if (a != null && a != this) {
+                        actors.add(a);
+                    }
+                }
+            }
+        }
+        return actors;
+    }
     
     public void processActors(ArrayList<Actor> actors){
-
+        
     }
 }
