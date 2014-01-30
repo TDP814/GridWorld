c08b714f8253015416ac974098aed4371a243364
Perkins <Perkinstd@s.dcsdk12.org>
2014-01-30 08:40:48 -0700

    Working

---
+++ critters/BlusterCritter.java
@@ -0,0 +1,14 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+import java.util.ArrayList;
+
+
+public class BlusterCritter extends Critter{
+    
+    public void processActors(ArrayList<Actor> actors){
+
+    }
+}
