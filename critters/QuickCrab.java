eb71ec86bdcd7bbc39ee7fcd37cff022cdad4b55
Perkins <Perkinstd@s.dcsdk12.org>
2014-02-04 08:19:47 -0700

    Added extends

---
+++ critters/QuickCrab.java
@@ -0,0 +1,17 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class QuickCrab extends CrabCritter
+{
+    public QuickCrab()
+    {
+        setColor(Color.RED);
+    }
+
+       
+}
